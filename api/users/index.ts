8c1d9e133ddbdbdbe6f075e9ba5b537af5ec1775
CristianoWorspace <cristianokuhn7@gmail.com>
2026-06-18 23:14:25 -0300

    Update index.ts

--- api/users/index.ts
+++ api/users/index.ts
@@ -1,4 +1,4 @@
-import usersHandler from "../usersHandler";
+import usersHandler from "../usersHandler.js";
 
 export default async function handler(req: any, res: any) {
   return usersHandler(req, res);
